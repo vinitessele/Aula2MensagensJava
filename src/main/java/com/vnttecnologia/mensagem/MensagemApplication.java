2afb359708333fd65a356527580be8aea3d85f78
vinitessele <vinicius_tessele@hotmail.com>
2024-02-27 15:12:55 -0300

    first commit

---
+++ src/main/java/com/vnttecnologia/mensagem/MensagemApplication.java
@@ -0,0 +1,41 @@
+package com.vnttecnologia.mensagem;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class MensagemApplication {
+
+	public static void main(String[] args) {
+		System.setProperty("java.awt.headless", "false");
+		SpringApplication.run(MensagemApplication.class, args);
+		//System.out.println("Olá mundo");
+		//JOptionPane.showMessageDialog(null,"Olá Mundo");
+		//create();
+		//read();
+		//update();
+		//delete();
+	@PostConstruct
+	public static void create() {
+		System.out.println("Cria um registro");			
+	}
+	@PostConstruct
+	public static void read() {
+		System.out.println("lê um registro");
+	}
+	@PostConstruct
+	public static void update() {
+		System.out.println("Altera um registro");
+	}
+	@PostConstruct
+	public static void delete() {
+		System.out.println("delete um registro");
+	}
+
+}
